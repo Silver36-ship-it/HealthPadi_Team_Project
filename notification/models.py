ad8fde2f1aed6e37442f5504d311218d6df19986
Feyipitan Igbowu <feyiigbowu@gmail.com>
2026-04-06 10:12:49 +0100

    feat: implement provider models and serializers for HealthPadi

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,22 @@
+from django.contrib.admin.options import FORMFIELD_FOR_DBFIELD_DEFAULTS
 from django.db import models
 
+from providers.models import Providers
+
+
 # Create your models here.
+
+class Notification(models.Model):
+    CHANNEL_TYPE = (
+        ('email', 'Email'),
+        ('sms', 'SMS'),
+    )
+    user_id = models.CharField(max_length=100, blank=False, FOREIGN_KEY=True)
+    provider_id = models.CharField(max_length=100, blank=False, FOREIGN_KEY=True)
+    message = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+    reference_id = models.CharField(max_length=100, blank=False, unique=True)
+    channel_type = models.CharField(max_length=20, choices=CHANNEL_TYPE)
+
+
--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,48 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
 
+from notification.models import Notification
+from providers.models import Providers
+
+
 # Create your views here.
+
+
+def create_user_notification(user):
+    notification = Notification.objects.create(
+        message=f"""
+        We are glad to have you here {user.first_name} {user.last_name}!\n   
+        Welcome to HealthPadi!   
+        """,
+    )
+    send_mail(
+        subject="Welcome to HealthPadi!",
+        message=notification.message,
+        from_email='',
+        recipient_list=[user.email],
+        fail_silently=True
+    )
+    notification.is_read = True
+    notification.save()
+
+def create_provider_notification(provider):
+    notification = Notification.objects.create(
+        message=f"""
+        Dear {provider.provider_name}, We are glad to partner with you!
+        
+        Welcome to HealthPadi!   
+        """,
+    )
+    send_mail(
+        subject="Welcome to HealthPadi Partner!",
+        message=notification.message,
+        from_email='',
+        recipient_list=[provider.provider_email],
+        fail_silently=True
+    )
+    notification.is_read = True
+    notification.save()
+
+def create_notification_for_provider_views(provider):
+    create_user_notification(provider)
+
